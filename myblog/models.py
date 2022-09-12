21959b0532a04db32248588cabbc19539c045daf
Lev E. Chechulin <l.e.chechulin@gmail.com>
2022-09-12 07:08:21 +0200

    better support for tags in models

--- myblog/models.py
+++ myblog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+class Tag(models.Model):
+    tag = models.CharField(max_length=32, unique=True)
+
 class Post(models.Model):
     title = models.CharField(max_length=255, unique=True)
     body = models.TextField()
     publish_date = models.DateField()
-    tags = models.CharField(max_length=64, null=True)
+    tags = Tag.tag
     url = models.CharField(max_length=32, null=True)
 
     class Meta:     
